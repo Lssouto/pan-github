d6681013015b53e58c44f65a4600660319e2573d
LSSouto <lssouto96@gmail.com>
2019-03-24 12:00:30 -0300

    add capitalize pipe and repo view sample

--- src/app/views/repo-list/repo-list.component.ts
+++ src/app/views/repo-list/repo-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GithubService } from 'src/app/services/github/github.service';
 import { RepoInfoModel } from 'src/app/model/repo-info.model';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-repo-list',
@@ -10,16 +11,34 @@ import { RepoInfoModel } from 'src/app/model/repo-info.model';
 export class RepoListComponent implements OnInit {
 
   private reposInformationArray: RepoInfoModel[];
+  private activeListClassStyle: string;
 
   constructor(private githubService: GithubService) {
     this.reposInformationArray = [];
+    this.activeListClassStyle = '__card';
   }
 
   ngOnInit() {
-    this.githubService.getRepoInformation()
+    // this.githubService.getRepoInformation()
+    //     .subscribe( (informationArray: RepoInfoModel[]) => {
+    //       console.log(JSON.stringify(informationArray))
+    //       this.reposInformationArray = informationArray;
+    //     });
+    this.getMockedValue()
         .subscribe( (informationArray: RepoInfoModel[]) => {
+          console.log(JSON.stringify(informationArray))
           this.reposInformationArray = informationArray;
         });
   }
 
+  private getMockedValue(): Observable<RepoInfoModel[]> {
+    const mockedValue = [{"name":"angular-cart","stargazers_count":0,"forks_count":0},{"name":"back_test","stargazers_count":0,"forks_count":0},{"name":"eotica-masonry-test","stargazers_count":1,"forks_count":0},{"name":"front-dev","stargazers_count":1,"forks_count":0},{"name":"jsp-eng-3","stargazers_count":0,"forks_count":0},{"name":"kbr-desafios","stargazers_count":0,"forks_count":0},{"name":"lssouto.github.io","stargazers_count":0,"forks_count":0},{"name":"mean","stargazers_count":0,"forks_count":0},{"name":"op-website-hugo","stargazers_count":0,"forks_count":0},{"name":"pan-github","stargazers_count":0,"forks_count":0},{"name":"react-api-connect","stargazers_count":0,"forks_count":0},{"name":"teste-front","stargazers_count":0,"forks_count":0},{"name":"the-foca","stargazers_count":0,"forks_count":0},{"name":"VanillaSPA-Example","stargazers_count":0,"forks_count":0},{"name":"vue-painel","stargazers_count":0,"forks_count":0},{"name":"vue-treinamento","stargazers_count":0,"forks_count":0},{"name":"web_test","stargazers_count":0,"forks_count":0},{"name":"yesod","stargazers_count":0,"forks_count":0}];
+    return of(mockedValue.map( mv => {
+      return new RepoInfoModel(mv);
+    }));
+  }
+
+  private toggleListClass(type: string) {
+    this.activeListClassStyle = type;
+  }
 }
--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { LoginComponent } from './login/login.component';
 import { RepoListComponent } from './repo-list/repo-list.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { CapitalizePipe } from '../pipes/capitalize/capitalize.pipe';
 
 @NgModule({
     declarations: [
         LoginComponent,
         RepoListComponent,
+        CapitalizePipe,
+    ],
+    imports: [
+        BrowserModule,
+        CommonModule,
     ],
     exports: [
         LoginComponent,
