e69ac9d0456f7e5deec708b9eea606497eeeb079
LSSouto <lssouto96@gmail.com>
2019-03-25 00:11:52 -0300

    fixing por

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ const certificate = {
 };
 
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = "0";
-const port = process.env.port || 8080;
+const port = process.env.PORT || 8080;
 https.createServer(certificate, app).listen(port, () => {
     console.log('Listening...')
 });
