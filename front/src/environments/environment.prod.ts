7e5d7d00cf84a8c9b70b595dae66b83986e136ad
LSSouto <lssouto96@gmail.com>
2019-03-25 10:29:55 -0300

    changin protocol to enviroment

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -3,4 +3,5 @@ export const environment = {
   port: 0,
   client_id: 'c63b5ef5b81e7610914a',
   client_secret: '32f6b8910137569d316eae627e3947b1aa37d77b',
+  protocol: 'https://'
 };
--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -7,6 +7,7 @@ export const environment = {
   port: 8080,
   client_id: 'c63b5ef5b81e7610914a',
   client_secret: '32f6b8910137569d316eae627e3947b1aa37d77b',
+  protocol: 'http://'
 };
 
 /*
